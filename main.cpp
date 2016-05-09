c0c4b77aac678a2806f7c1feeb4215fde05b9d74
mieszko <mieszkosluzewski@gmail.com>
2016-05-09 23:40:05 +0200

    Simple menu

--- main.cpp
+++ main.cpp
@@ -4,15 +4,54 @@
 
 using namespace std;
 
+
 int main() {
 
-	float f = 4;
-	float f2 = 2;
-	Model model(f);
-	Model model2(f2);
-	Model *model3 = model.subtract(model2);
-	cout << (*model3).get_number();
-	cin.get();
+    float f, f2;
+    Model *model3;
 
+
+    int m;
+    char sign0;
+    do{
+        cout << "MENU \n[1] - dodawanie \n[2] - odejmowanie  ";
+        cin >> m;
+        cout << endl;
+
+        switch(m)
+        {
+            case 1: {
+                cout << "Podaj pierwszy składnik dodawania: ";
+                cin >> f;
+                Model model(f);
+                cout << "Podaj drugi składnik dodawania: ";
+                cin>> f2;
+                Model model2(f2);
+                model3 = model.add(model2);
+                cout << "Wynik dodawania " << f << " + " << f2 << " to " << (*model3).get_number();
+                break;
+            }
+            case 2: {
+                cout << "Podaj odjemną: ";
+                cin >> f;
+                Model model(f);
+                cout << "Podaj odjemnik: ";
+                cin>> f2;
+                Model model2(f2);
+                model3 = model.subtract(model2);
+                cout << "Wynik odejmowania " << f << " - " << f2 << " to " << (*model3).get_number();
+                break;
+            }
+            /*case 3:
+                break;
+
+            case 4:
+                break;*/
+            default:break;
+
+        }
+        cout << endl << "Czy chcesz zakonczyc program(t/n)?"; cin>>sign0;
+        cout << endl;
+    }while(sign0 !='t');
     return 0;
 }
